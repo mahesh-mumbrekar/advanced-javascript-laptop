246e881fae0296b2d7f979272f0eca2a80c4e480
mahesh <mahesh.mumbrekar@gmail.com>
2023-07-02 03:45:27 +0100

    rest parameters

--- function.js
+++ function.js
@@ -18,3 +18,16 @@ function addNumbers(num1, num2=5) {
  
 let add = addNumbers(1);
 console.log(add);
+
+//rest parameters
+
+function sum(...numbers) {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return total;
+}
+
+console.log(sum(1, 2, 3));  // Output: 6
+console.log(sum(4, 5, 6, 7));  // Output: 22
