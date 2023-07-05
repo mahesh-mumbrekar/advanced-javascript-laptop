2895dd12b9e52961da6ec98ce166b3869baf08b5
mahesh <mahesh.mumbrekar@gmail.com>
2023-07-05 19:04:26 +0100

    asyncronus code  readFile which is use to perform other code before reading file (imp)

---
+++ async.js
@@ -0,0 +1,17 @@
+const fs=require('fs');
+
+
+function readFile(){
+  let fileData;
+
+  fs.readFile('data.txt',function(error,fileData){
+     console.log('File parsing done!');
+     console.log(fileData.toString());
+  })
+  
+  console.log('hi there!');
+}
+
+
+
+readFile();
